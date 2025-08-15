254ce8e3cf60ca9c2818a5b23df504b52ab32350
SarkarDevelopres <sarkarindustires77@gmail.com>
2025-08-15 19:19:55 +0530

    Added HTML

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/connectDB');
+const path = require('path');
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,10 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth.routes'));
